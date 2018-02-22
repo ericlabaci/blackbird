96c8c0dc1a5f57b8c1884cf72c8bfc63a8da526a
Raphael Carletti <raphael.carletti@vistajet.com>
2018-02-22 10:24:48 -0300

    Add constants file and main images for the app

---
+++ Blackbird/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Blackbird
+//
+//  Created by Raphael Carletti on 2/22/18.
+//  Copyright © 2018 Blackbird. All rights reserved.
+//
+
+import UIKit
+
+class Color {
+    static let secondaryColor = UIColor(red: 139/255, green: 219/255, blue: 231/255, alpha: 1.0)
+}
+
+class Storyboard {
+    
+}
+
+class ViewControllers {
+    
+}
+
+class Firebase {
+    
+}
